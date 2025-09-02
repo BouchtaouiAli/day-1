c6a337d34015a87a007ef9d9544edf5b4dcfcbd7
BouchtaouiAli <alibouchtaoui24@gmail.com>
2025-09-02 11:45:50 +0100

    ex7

---
+++ exercices/main ex 7.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num1;
+    printf("veuiller entrer vote nobmbre: \n");
+    scanf("%d",&num1);
+    if(num1==0){
+        printf("le nombre est pair");
+    }
+    else{
+        printf("le nombre est impair");
+    }
+    return 0;
+}
