c6fa9c78cac0dab35ef2bf14f3ebba453043c1ab
BouchtaouiAli <alibouchtaoui24@gmail.com>
2025-09-02 11:29:10 +0100

    ex3

---
+++ exercices/main ex3.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    float km,y;
+    printf("enter km : \n");
+    scanf("%f",&km);
+    y = km * 1093.61;
+    printf("la distance en yards est: %.2f y\n",y);
+
+
+    return 0;
+}
